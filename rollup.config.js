be5af346bcc011dc8548e04c1c5e1ea643998f39
Ben Farrell <ben@benfarrell.com>
2021-01-12 22:07:23 -0800

    added rollup clean step to build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import html from '@web/rollup-plugin-html';
 import copy from 'rollup-plugin-copy';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import clean from 'rollup-plugin-clean';
 
 export default {
     input: 'index.html',
     output: { dir: 'dist' },
     plugins: [
+        clean(),
         nodeResolve(),
         html(),
         copy({
